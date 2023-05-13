63a6c40e3023e0598256088084c964d977bcdef2
AVIMIR <90897378+AVIMIR@users.noreply.github.com>
2023-05-13 14:30:55 +0100

    Проба пера в tkinter

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import tkinter as tk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import ttkbootstrap as ttk
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# функция для кнопки
+def confirm_func():
+    km = entry_var.get()
+    output_var.set(str(1.61 * km))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Конфигурация приложения
+app = ttk.Window(themename='journal')
+app.title('My application')
+app.geometry('300x150')
+
+# Заголовок
+main_label = ttk.Label(master=app, text='Мили в километры', font='TimesNewRoman 24 bold')
+main_label.pack()
+
+# контейнер ввода
+entry_space_container = ttk.Frame(master=app)
+entry_space_container.pack()
+
+# поле ввода
+entry_var = ttk.IntVar()
+entry_frame = ttk.Entry(master=entry_space_container, textvariable=entry_var)
+entry_frame.pack(side='left')
+
+# кнопка ввода
+confirm_button = ttk.Button(master=entry_space_container, text=('подтвердить'), command=confirm_func)
+confirm_button.pack(side='right')
+
+# вывод результата работы алгоритма на экран
+output_var = ttk.StringVar()
+output_label = ttk.Label(master=app, text='Здесь будет вывод', font='TimesNewRoman 16', textvariable=output_var)
+output_label.pack()
+
+
+
+app.mainloop()
